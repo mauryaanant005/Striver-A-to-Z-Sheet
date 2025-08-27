0bd9cb307fbf742ab2c14e19a941ad95447c1e72
Anant Maurya <mauryaanant005@gmail.com>
2025-08-28 00:59:31 +0530

    Finding factorial of n number using Recursion

---
+++ Day 7/FactorialRecursion.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class FactorialRecursion {
+        public static int Fact(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return (n * Fact(n - 1));
+
+    }
+
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Enter  a Number: ");
+        int n = inp.nextInt();
+        int result =Fact(n);
+        System.out.println("The sum of 1 to " + n + " is " + result);
+    }
+}
+
+
