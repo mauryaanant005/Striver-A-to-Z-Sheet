0459fe3c1f1032cfad60018331721bf13840a282
Anant Maurya <mauryaanant005@gmail.com>
2025-09-19 19:10:30 +0530

    Its working

--- Day 8/SelectionDes.java
+++ Day 8/SelectionDes.java
@@ -1,3 +1,43 @@
+import java.util.Scanner;
+
 public class SelectionDes {
-    
+    static Scanner inp = new Scanner(System.in);
+
+    public static void DescendingOrder(int arr[],int n){
+        for(int i=0;i<n-1;i++){
+            int LI=i;
+            int j,temp ;
+            for( j=i+1;j<n;j++){
+                if(arr[j]>arr[LI]){
+                    LI=j;
+                }
+            }
+            temp = arr[i];
+            arr[i]=arr[LI];
+            arr[LI]=temp;  
+        }
+
+        System.out.print("Sorted Array in Descending order: ");
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+    public static void main(String[] args) {
+        System.out.print("Enter the number of elements: ");
+        int n = inp.nextInt();
+        System.out.println("Enter the elments:  ");
+        int arr[]=new int[n];
+
+        for(int i =0;i<n;i++){
+            arr[i]=inp.nextInt();
+        }
+
+        System.out.print("Unorted Array ");
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+        DescendingOrder(arr,n);
+        
+    }
 }
